dd097c59edc25703edb97145c356d438dcdef626
dunu rex <dunuchukwuemeka22@gmail.com>
2026-03-25 23:13:21 +0100

    added the tech stack section

--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -5,4 +5,5 @@ from Projects.models import Project
 
 def home(request):
     obj = Project.objects.all()
+    obj3 = obj[:3]
     return render(request, 'home.html', {'projects': obj})
--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,12 @@
 const currentPage = window.location.pathname // current page location
-console.log(currentPage);
 
 const pages = {
     '/': document.getElementById('home'),
     '/projects/': document.getElementById('projects'),
-    '/about/': document.getElementById('about')
+    '/about/': document.getElementById('about'),
+    '/contact/': document.getElementById('contact')
 }
 
 if (pages[currentPage]){
-    console.log('home page!')
-    console.log(pages[currentPage])
     pages[currentPage].classList.add('loc');
 }
