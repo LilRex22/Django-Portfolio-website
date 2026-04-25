66477d13f2f9f096d34d2af62d0998529d619e58
dunu rex <dunuchukwuemeka22@gmail.com>
2026-04-25 01:02:48 +0100

    started GSAP animations (header)

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,31 @@ const pages = {
 if (pages[currentPage]){
     pages[currentPage].classList.add('loc');
 }
+
+
+// GSAP Animations 😪
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll("#dy > *");
+    const items2 = document.querySelectorAll("#pf > *");
+    const absItem = document.querySelectorAll('#pf .position-absolute');
+
+    const tl = gsap.timeline();
+
+    tl.from(items2, {
+        opacity: 0,
+        scale: 0,
+        duration: 1,
+        stagger: 0.2,
+        ease: "back.out(1.7)"
+    });
+
+    tl.from(items, {
+        y: (i) => 50 * (i + 3),
+        opacity: 0,
+        duration: 1,
+        stagger: 0.2,
+        ease: "back.out(1.7)"
+    });
+
+
+});
