36cadfc90363acfefed342a02e4a5912f05c131d
dunu rex <dunuchukwuemeka22@gmail.com>
2026-04-30 02:10:43 +0100

    email test

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -144,15 +144,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Our Email Settings (Brevo SMTP)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp-relay.brevo.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=None)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=None)
-DEFAULT_FROM_EMAIL = "Code.Rex.com"
-EMAIL_TIMEOUT = 30
+# # Our Email Settings (Brevo SMTP)
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp-relay.brevo.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=None)
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=None)
+# DEFAULT_FROM_EMAIL = "Code.Rex.com"
+# EMAIL_TIMEOUT = 30
 
 # Cloudinary settings
 CLOUDINARY_STORAGE = {
